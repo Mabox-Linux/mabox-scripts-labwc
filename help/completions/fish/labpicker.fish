d4b95f3ec85dbec554b7bb4666cd9111a595eb97
Daniel Napora <napcok@gmail.com>
2025-01-04 14:05:50 +0100

    upload

---
+++ help/completions/fish/labpicker.fish
@@ -0,0 +1,8 @@
+complete -c labpicker -f
+complete -c labpicker -s p -l print -d "Print to stdout"
+complete -c labpicker -s d -l draw -d "Draw a colored block"
+complete -c labpicker -s e -l escape -d "Print shell escape sequences"
+complete -c labpicker -s c -l copy -d "Copy to clipboard"
+complete -c labpicker -s n -l notify -d "Send a notification"
+complete -c labpicker -s h -l help -d "Show help message and quit"
+complete -c labpicker -s v -l version -d "Show version number and quit"
